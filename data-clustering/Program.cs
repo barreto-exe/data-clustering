868146d735440991f6dbcdd775a6288ba2fb2bcc
Luis Barreto <luis.barreto.marin@gmail.com>
2023-01-15 14:09:25 -0400

    Add button for creating random rows

--- data-clustering/Program.cs
+++ data-clustering/Program.cs
@@ -4,7 +4,8 @@
 Console.WriteLine(SimilarityTool.CompareStrings("JosE Alberto    ", "jose alberto"));
 Console.WriteLine(SimilarityTool.CompareStrings("JosE Albertooooooo    ", "jose alberto"));
 Console.WriteLine(SimilarityTool.CompareStrings("JosE 'chucho' Alberto    ", "chucho"));
-Console.WriteLine(SimilarityTool.CompareStrings("JosE Alberto    ", "jose alberto"));
+Console.WriteLine(SimilarityTool.CompareStrings("es dlsciniipado.", "es disciilpnado."));
+Console.WriteLine(SimilarityTool.CompareStrings("es dlsciniipado.", "disciilpnado."));
 Console.WriteLine(SimilarityTool.CompareStrings("JosE Alberto    ", "jose alberto"));
 Console.WriteLine(SimilarityTool.CompareStrings("JosE Alberto    ", "jose alberto"));
 Console.WriteLine(SimilarityTool.CompareStrings("JosE Alberto    ", "jose alberto"));
--- data-filling/Views/MainWindow.xaml.cs
+++ data-filling/Views/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using data_filling.Models;
 using Microsoft.Win32;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,6 +27,10 @@ namespace data_filling
     {
         public string DbPath { get; set; }
 
+        private string namesJson = "[\r\n  \"Castro, Stone W.\",\r\n  \"Shoshana X Sebastian\",\r\n  \"Daria Y Fernandez\",\r\n  \"Jonas O Paz\",\r\n  \"Lilah Juan\",\r\n  \"Sepulveda\",\r\n  \"Perez\",\r\n  \"Zephr Pia\",\r\n  \"Guy P Hernandez\",\r\n  \"Bentlee, Hilel T.\",\r\n  \"Chiquita Figueroa\",\r\n  \"Fernandez, Stacey V.\"]";
+
+        private string virtuesJson = "[\r\n  \"es disciplinado.\",\r\n  \"bueno amigo\",\r\n  \"Carismático\",\r\n  \"sociable\",\r\n  \"persona responsable\"]\r\n";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -41,6 +47,10 @@ namespace data_filling
         }
 
         private async void BtnInsertar_Click(object sender, RoutedEventArgs e)
+        {
+            await InsertPollRow();
+        }
+        private async Task InsertPollRow()
         {
             int sexo = (bool)BtnFemenino.IsChecked ? 1 : 2;
             int semestre = Convert.ToInt32(((ComboBoxItem)CbSemestre.SelectedItem).DataContext);
@@ -65,6 +75,149 @@ namespace data_filling
             Poll poll = new(sexo, semestre, carrera, conoceEIU, actividadEIU, cualidades, postulados);
 
             await poll.InsertDbRow(DbPath);
+
+            ResetAllControls(StkMain);
+        }
+
+        private async void BtnRandom_Click(object sender, RoutedEventArgs e)
+        {
+            await FillRandomAnswers();
+        }
+        private async Task FillRandomAnswers()
+        {
+            Random random = new();
+
+            CbSemestre.SelectedIndex = random.Next(0, 10);
+            CbCarrera.SelectedIndex = random.Next(0, 14);
+            CbActividad.SelectedIndex = random.Next(0, 8);
+            if (random.Next(1, 3) == 1)
+            {
+                BtnFemenino.IsChecked = true;
+            }
+            else
+            {
+                BtnMasculino.IsChecked = true;
+            }
+            if (random.Next(1, 3) == 1)
+            {
+                BtnSiConoce.IsChecked = true;
+            }
+            else
+            {
+                BtnNoConoce.IsChecked = true;
+            }
+
+            var virtuesList = JsonConvert.DeserializeObject<string[]>(virtuesJson);
+            int virtues = virtuesList?.Length ?? 0;
+
+            var namesList = JsonConvert.DeserializeObject<string[]>(namesJson);
+            int names = namesList?.Length ?? 0;
+
+            TextCualidad1.Text = Randomize(virtuesList[random.Next(0, virtues)]);
+            TextCualidad2.Text = Randomize(virtuesList[random.Next(0, virtues)]);
+            TextCualidad3.Text = Randomize(virtuesList[random.Next(0, virtues)]);
+
+            TextEstudiante1.Text = Randomize(namesList[random.Next(0, names)]);
+            TextEstudiante2.Text = Randomize(namesList[random.Next(0, names)]);
+            TextEstudiante3.Text = Randomize(namesList[random.Next(0, names)]);
+        }
+
+        private string Randomize(string data)
+        {
+            Random random = new();
+            bool gonnaDoIt;
+
+            var dataArray = data.ToCharArray();
+
+            //All "gonnaDoIt" sections determine to do
+            //the messing or not by a probability of 1 out of 20.
+            int probability = 21;
+
+            for (int i = 0; i < dataArray.Length; i++)
+            {
+                //Randomly swaps characters
+                gonnaDoIt = random.Next(1, probability) == 5;
+                if (gonnaDoIt)
+                {
+                    int randomIndex = random.Next(0, dataArray.Length);
+
+                    char temp = dataArray[randomIndex];
+
+                    dataArray[randomIndex] = dataArray[i];
+
+                    dataArray[i] = temp;
+                }
+
+                //Randomly changes characters to upper or lower
+                gonnaDoIt = random.Next(1, probability) == 5;
+                if (gonnaDoIt)
+                {
+                    int randomIndex = random.Next(0, dataArray.Length);
+                    if(random.Next(1,3) == 1)
+                    {
+                        dataArray[randomIndex] = dataArray[randomIndex].ToString().ToUpper()[0];
+                    }
+                    else
+                    {
+                        dataArray[randomIndex] = dataArray[randomIndex].ToString().ToLower()[0];
+                    }
+                }
+            }
+
+            data = new string(dataArray);
+
+            //Randomly add spaces
+            gonnaDoIt = random.Next(1, probability) == 5;
+            if (gonnaDoIt)
+            {
+                int randomIndex = random.Next(0, dataArray.Length);
+
+                int spacesNumber = random.Next(1, 4);
+
+                data = data.Insert(randomIndex, new string(' ', spacesNumber));
+            }
+
+            return data;
+        }
+
+        private async void BtnCrear100_Click(object sender, RoutedEventArgs e)
+        {
+            for(int i = 0; i < 30; i++)
+            {
+                await FillRandomAnswers();
+                await InsertPollRow();
+            }
+        }
+
+
+        private void ResetAllControls(Panel mainPanel)
+        {
+            foreach (var item in mainPanel.Children)
+            {
+                if (item is Panel panel)
+                {
+                    ResetAllControls(panel);
+                }
+
+                if (item is Control control)
+                {
+                    if (control is TextBox textBox)
+                    {
+                        textBox.Text = null;
+                    }
+
+                    if (control is ComboBox comboBox)
+                    {
+                        if (comboBox.Items.Count > 0)
+                            comboBox.SelectedIndex = 0;
+                    }
+
+                    if (control is RadioButton button)
+                    {
+                        button.IsChecked = false;
+                    }
+                }
+            }
         }
     }
 }
