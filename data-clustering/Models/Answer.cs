b2f02778a3a6374ed7e7ab65d855453b8e58b650
Luis Barreto <luis.barreto.marin@gmail.com>
2023-01-15 19:52:06 -0400

    Add methods for creating results 1, 2 & 3

--- data-clustering/Models/Answer.cs
+++ data-clustering/Models/Answer.cs
@@ -2,9 +2,32 @@
 {
     public class Answer
     {
-        public int AnswerNumber { get; set; }
+        public int PollNumber { get; set; }
         public int QuestionNumber { get; set; }
-        public int OptionNumber { get; set; }
-        public string? Text { get; set; }
+        public string? QuestionTitle { get; set; }
+        public int AnswerNumber { get; set; }
+        public string? AnswerTitle { get; set; }
+        public string? AnswerText { get; set; }
+
+        public override string? ToString()
+        {
+            return $"Poll: {PollNumber} - Question: {QuestionNumber} - AnswerNumer: {AnswerNumber} - AnswerText: {AnswerText}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Answer answer &&
+                   PollNumber == answer.PollNumber &&
+                   QuestionNumber == answer.QuestionNumber &&
+                   QuestionTitle == answer.QuestionTitle &&
+                   AnswerNumber == answer.AnswerNumber &&
+                   AnswerTitle == answer.AnswerTitle &&
+                   AnswerText == answer.AnswerText;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(PollNumber, QuestionNumber, QuestionTitle, AnswerNumber, AnswerTitle, AnswerText);
+        }
     }
 }
--- data-clustering/Program.cs
+++ data-clustering/Program.cs
@@ -1,6 +1,10 @@
 ﻿using DataClustering.Models;
 using Microsoft.Data.Sqlite;
 using System.Data;
+using System.Linq;
+using System.Collections.Generic;
+using DataClustering;
+using System.Text.RegularExpressions;
 
 string dbPath = GetDbPath();
 var arguments = GetArguments();
@@ -10,7 +14,127 @@ GetResult1(answerList, arguments);
 GetResult2(answerList, arguments);
 GetResult3(answerList, arguments);
 
-#region Functions
+#region Result Functions
+static async Task GetResult1(List<Answer> answers, Dictionary<string, object> args)
+{
+    bool dismissAltAnswers = (bool) GetArgsValue(args, "dismissAltAnswers");
+
+    List<Answer> answersQ4;
+    if(dismissAltAnswers)
+    {
+        answersQ4 = (from Answer a in answers
+                                where a.QuestionNumber == 4 && (a.AnswerTitle == "Sí" || a.AnswerTitle == "No")
+                                select a).ToList();
+    }
+    else
+    {
+        answersQ4 = (from Answer a in answers
+                                where a.QuestionNumber == 4
+                                select a).ToList();
+    }
+    
+    int totalAnswered = answersQ4.Count;
+
+    var countYes = answersQ4.Where(x => x.AnswerTitle == "Sí").Count();
+    double percentageYes = (double)countYes / (double)totalAnswered;
+
+    var countNo = answersQ4.Where(x => x.AnswerTitle == "No").Count();
+    double percentageNo = (double)countNo / (double)totalAnswered;
+
+    //Counting times alt answers were given
+    double percentageAlt = 0;
+    if (!dismissAltAnswers)
+    {
+        var countAlt = answersQ4.Where(x => x.AnswerTitle != "Sí" && x.AnswerTitle != "No").Count();
+        percentageAlt = (double)countAlt / (double)totalAnswered;
+    }
+}
+static async Task GetResult2(List<Answer> answers, Dictionary<string, object> args)
+{
+    var answersQ6 = GetAnswersFromOpenQuestion(answers, 6);
+    double percentage = (double) GetArgsValue(args, "percentage");
+
+    Dictionary<Answer, int> answersApparitions = new();
+    List<Answer> alreadyCounted = new();
+    try
+    {
+        do
+        {
+            //Get the first answer of the list that hasn't been clustered yet
+            Answer answer = answersQ6.FirstOrDefault(x => !alreadyCounted.Contains(x));
+            alreadyCounted.Add(answer);
+
+            int apparitions = 1;
+
+            foreach (Answer subAnswer in answersQ6)
+            {
+                if (alreadyCounted.Contains(subAnswer)) continue;
+
+                if (SimilarityTool.CompareStrings(answer.AnswerText, subAnswer.AnswerText) >= percentage)
+                {
+                    alreadyCounted.Add(subAnswer);
+                    apparitions++;
+                }
+            }
+
+            //Put text in lower and capitalize the first letter
+            answer.AnswerText = answer.AnswerText.ToLower().Trim();
+            string first = answer.AnswerText[0].ToString().ToUpper();
+            answer.AnswerText = first + answer.AnswerText.Substring(1);
+
+            answersApparitions.Add(answer, apparitions);
+        } while (alreadyCounted.Count < answersQ6.Count);
+    }
+    catch (Exception ex)
+    {
+
+    }
+}
+static async Task GetResult3(List<Answer> answers, Dictionary<string, object> args)
+{
+    List<Answer> answersQ7 = GetAnswersFromOpenQuestion(answers, 7);
+
+    double percentage = (double) GetArgsValue(args, "percentage");
+
+    Dictionary<Answer, int> answersApparitions = new();
+    List<Answer> alreadyCounted = new();
+    try
+    {
+        do
+        {
+            //Get the first answer of the list that hasn't been clustered yet
+            Answer answer = answersQ7.FirstOrDefault(x => !alreadyCounted.Contains(x));
+            alreadyCounted.Add(answer);
+
+            int apparitions = 1;
+
+            foreach (Answer subAnswer in answersQ7)
+            {
+                if (alreadyCounted.Contains(subAnswer)) continue;
+
+                if (SimilarityTool.CompareStrings(answer.AnswerText, subAnswer.AnswerText) >= percentage)
+                {
+                    alreadyCounted.Add(subAnswer);
+                    apparitions++;
+                }
+            }
+
+            //Put text in lower and capitalize the first letter
+            answer.AnswerText = answer.AnswerText.ToLower().Trim();
+            string first = answer.AnswerText[0].ToString().ToUpper();
+            answer.AnswerText = first + answer.AnswerText.Substring(1);
+
+            answersApparitions.Add(answer, apparitions);
+        } while (alreadyCounted.Count < answersQ7.Count);
+    }
+    catch (Exception ex)
+    {
+
+    }
+}
+#endregion
+
+#region Other Functions
 static string GetDbPath()
 {
     //Console.WriteLine("Indique el path de la BD:");
@@ -22,17 +146,19 @@ Dictionary<string, object> GetArguments()
 {
     Console.WriteLine(
         "Desea desechar respuestas alternativas en las preguntas cerradas? [S/N]");
-    bool considerAltAnswers = Console.ReadKey().Key == ConsoleKey.S;
+    bool dismissAltAnswers = Console.ReadKey().Key == ConsoleKey.S;
+    Console.WriteLine("\n\n");
 
     Console.WriteLine(
         $"Indique el margen de similitud aceptado para las respuestas abiertas. " +
         $"Siendo (0) muy flexible y (100) muy estricto");
-    int percentage = Convert.ToInt32(Console.ReadLine());
+    double percentage = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("\n\n");
 
     return new Dictionary<string, object>
     {
-        [nameof(considerAltAnswers)] = considerAltAnswers,
-        [nameof(percentage)] = percentage,
+        [nameof(dismissAltAnswers)] = dismissAltAnswers,
+        [nameof(percentage)] = (double)percentage / 100d,
     };
 }
 static async Task<List<Answer>> GetAnswerList(string dbPath)
@@ -44,7 +170,14 @@ static async Task<List<Answer>> GetAnswerList(string dbPath)
 
     List<Answer> answerList = new();
 
-    query = "SELECT * FROM Respuesta";
+    query =
+        "SELECT " +
+            "r.num_encuesta, r.num_pregunta, p.titulo as tit_preg, " +
+            "r.num_opcion as num_resp, o.titulo as tit_resp, r.respuesta as texto_resp\n" +
+        "FROM Respuesta r\n" +
+        "INNER JOIN Pregunta p ON r.num_pregunta = p.num_pregunta\n" +
+        "INNER JOIN Opcion o ON r.num_opcion = o.num_opcion AND r.num_pregunta = o.num_pregunta\n" +
+        "ORDER BY r.num_encuesta, r.num_pregunta, r.num_opcion";
     using (var command = new SqliteCommand(query, conn))
     {
         var reader = command.ExecuteReader();
@@ -53,10 +186,12 @@ static async Task<List<Answer>> GetAnswerList(string dbPath)
         {
             Answer answer = new()
             {
-                AnswerNumber = reader.GetInt32("num_encuesta"),
+                PollNumber = reader.GetInt32("num_encuesta"),
+                AnswerNumber = reader.GetInt32("num_resp"),
+                AnswerTitle = reader.GetString("tit_resp"),
                 QuestionNumber = reader.GetInt32("num_pregunta"),
-                OptionNumber = reader.GetInt32("num_opcion"),
-                Text = reader.IsDBNull("respuesta") ? null : reader.GetString("respuesta"),
+                QuestionTitle = reader.GetString("tit_preg"),
+                AnswerText = reader.IsDBNull("texto_resp") ? null : reader.GetString("texto_resp"),
             };
 
             answerList.Add(answer);
@@ -67,16 +202,22 @@ static async Task<List<Answer>> GetAnswerList(string dbPath)
 
     return answerList;
 }
-static async Task GetResult1(List<Answer> answers, Dictionary<string, object> args)
+static List<Answer> GetAnswersFromOpenQuestion(List<Answer> answers, int questionNumber)
 {
-
+    return (from Answer a in answers
+            where a.QuestionNumber == questionNumber && !string.IsNullOrEmpty(a.AnswerText)
+            select new Answer
+            {
+                PollNumber = a.PollNumber,
+                AnswerNumber = a.AnswerNumber,
+                AnswerTitle = a.AnswerTitle,
+                QuestionTitle = a.QuestionTitle,
+                QuestionNumber = a.QuestionNumber,
+                AnswerText = Regex.Replace(a.AnswerText, @"[!@#$%^-_><&*0-9]", String.Empty),
+            }).ToList();
 }
-static async Task GetResult2(List<Answer> answers, Dictionary<string, object> args)
+static object GetArgsValue(Dictionary<string, object> args, string parameter)
 {
-
-}
-static async Task GetResult3(List<Answer> answers, Dictionary<string, object> args)
-{
-
+    return args.FirstOrDefault(x => x.Key == parameter).Value;
 }
 #endregion
